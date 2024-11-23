e6b9a7aa5bee601bb0d925c51b69419fb443afd9
unknown <m.mehdi.paikane@gmail.com>
2024-11-23 23:07:26 +0330

    created layout

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Weather from "./Weather";
+import Calendar from "./Calendar";
+import "./News.css";
 
 function News() {
   return (
-    <div>News</div>
-  )
+    <div className="news">
+      <header className="news-header">Header</header>
+      <div className="news-content">
+        <div className="navbar">
+          <div className="user">User</div>
+          <nav className="categories">Categories</nav>
+        </div>
+        <div className="news-section">
+          <div className="headline">headline</div>
+          <div className="news-grid">news-grid</div>
+        </div>
+        <div className="my-blogs"></div>
+        <div className="weather-calendar">
+          <Weather />
+          <Calendar />
+        </div>
+      </div>
+      <footer className="news-footer">Footer</footer>
+    </div>
+  );
 }
 
-export default News
+export default News;
