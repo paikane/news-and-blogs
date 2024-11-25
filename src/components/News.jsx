b2b8ae5e20476758cfad7462ff42a64359299bd0
unknown <m.mehdi.paikane@gmail.com>
2024-11-25 20:32:15 +0330

    custom navbar

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useId } from "react";
 import Weather from "./Weather";
 import Calendar from "./Calendar";
 import "./News.css";
+import userImg from "../assets/images/user.jpg";
 
 function News() {
   return (
@@ -12,15 +13,49 @@ function News() {
           <form>
             <input type="text" placeholder="Search News..." />
             <button type="submit">
-            <i class="fa-solid fa-magnifying-glass"></i>
+              <i class="fa-solid fa-magnifying-glass"></i>
             </button>
           </form>
         </div>
       </header>
       <div className="news-content">
         <div className="navbar">
-          <div className="user">User</div>
-          <nav className="categories">Categories</nav>
+          <div className="user">
+            <img src={userImg} alt="" />
+            <p>Mery's Blog</p>
+          </div>
+          <nav className="categories">
+            <h1 className="nav-heading">categories</h1>
+            <div className="nav-links">
+              <a href="" className="nav-link">
+                General
+              </a>
+              <a href="" className="nav-link">
+                World
+              </a>
+              <a href="" className="nav-link">
+                Technology
+              </a>
+              <a href="" className="nav-link">
+                Entertainment
+              </a>
+              <a href="" className="nav-link">
+                Sport
+              </a>
+              <a href="" className="nav-link">
+                Science
+              </a>
+              <a href="" className="nav-link">
+                Health
+              </a>
+              <a href="" className="nav-link">
+                Nation
+              </a>
+              <a href="" className="nav-link">
+                Bookmarks <i class="fa-regular fa-bookmark"></i>
+              </a>
+            </div>
+          </nav>
         </div>
         <div className="news-section">
           <div className="headline">headline</div>
